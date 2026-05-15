28e7eb1d3e73a1897b3cacbe2292f24d019f3aa9
Lauren Blue <kupt5ova.tatyana@yandex.ru>
2026-05-15 12:46:54 +0200

    first changes

---
+++ ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Sofia Noelle on 15.05.26.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
